02e0660a3bc909aaf7092d02058c1bd35810f319
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2023-01-02 22:39:12 -0800

    Quick edit on the about me section

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -818,7 +818,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59259" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55991" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
